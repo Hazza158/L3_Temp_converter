6ae9f3474e3446d5bf7616051e0d944ef4bb16da
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2024-03-01 14:56:15 +1300

    halfway thru help stuff

--- 06_help_GUI_V1.py
+++ 06_help_GUI_V1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial   # To prevent unwanted windows
 
 
 class Converter:
@@ -32,10 +33,19 @@ class Converter:
 
 class DisplayHelp:
 
-    def __init__(self):
-        background = "#ffe6cc"
+    def __init__(self, partner):
 
+        # setup dialogue box and background colour
+        background = "#ffe6cc"
         self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # if users press cross at top, closes help and
+        # 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
 
         self.help_frame = Frame(self.help_box, width=300, height=200,
                                 bg=background)
@@ -61,6 +71,21 @@ class DisplayHelp:
                                      text=help_text, wrap=350,
                                      justify="left")
         self.help_text_label.grid(row=1, padx=10)
+
+        self.dismiss_button = Button(self.help_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help,
+                                                     partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+
+        # closes help dialogue (used by button and x at top of dialogue)
+        def close_help(self, partner):
+            # put help button back to normal
+            partner.to_help_button.config(state=NORMAL)
+            self.help_box.destroy()
 
 
 

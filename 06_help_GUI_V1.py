6420c3cb4e55bdc21ff1371cae708d2d1dc31cf8
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2024-03-05 09:44:13 +1300

    working help GUI

--- 06_help_GUI_V1.py
+++ 06_help_GUI_V1.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from functools import partial   # To prevent unwanted windows
+from functools import partial  # To prevent unwanted windows
 
 
 class Converter:
@@ -7,11 +7,10 @@ class Converter:
     def __init__(self):
         # common format for all buttons
         # Arial size 14 bold, with white text
-
         button_font = ("Arial", "12", "bold")
         button_fg = "#FFFFFF"
 
-        # set up GUI frame
+        # Set up GUI Frame
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
@@ -26,9 +25,8 @@ class Converter:
                                      command=self.to_help)
         self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
 
-    @staticmethod
-    def to_help():
-        DisplayHelp()
+    def to_help(self):
+        DisplayHelp(self)
 
 
 class DisplayHelp:
@@ -42,30 +40,33 @@ class DisplayHelp:
         # disable help button
         partner.to_help_button.config(state=DISABLED)
 
-        # if users press cross at top, closes help and
+        # If users press cross at top, closes help and
         # 'releases' help button
         self.help_box.protocol('WM_DELETE_WINDOW',
-                               partial(self.close_help, partner))
+                               partial(self.close_help,partner))
 
-        self.help_frame = Frame(self.help_box, width=300, height=200,
+        self.help_frame = Frame(self.help_box, width=300,
+                                height=200,
                                 bg=background)
         self.help_frame.grid()
 
-        self.help_heading_label = Label(self.help_frame, bg=background,
+        self.help_heading_label = Label(self.help_frame,
+                                        bg=background,
                                         text="Help / Info",
                                         font=("Arial", "14", "bold"))
         self.help_heading_label.grid(row=0)
 
-        help_text = "To use the program, simply enter the temperature" \
-                    "you wish to convert and then choose to convert" \
-                    "to either degrees Celcius (centigrade) or" \
-                    "Fahrenheit.. \n\n" \
-                    " Note that -273 degrees C" \
-                    "(-459) is absolute zero (the coldest possible temperature)." \
-                    "If you try to convert a temperature" \
-                    "that is less than -273 degrees C, you will get an error message. \n\n\"" \
-                    "To see your" \
-                    "calculation history and export it to a text" \
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert " \
+                    "to either degrees Celsius (centigrade) or " \
+                    "Fahrenheit..  \n\n" \
+                    " Note that -273 degrees C " \
+                    "(-459 F) is absolute zero (the coldest possible " \
+                    "temperature).  If you try to convert a " \
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n " \
+                    "To see your " \
+                    "calculation history and export it to a text " \
                     "file, please click the 'History / Export' button."
         self.help_text_label = Label(self.help_frame, bg=background,
                                      text=help_text, wrap=350,
@@ -81,13 +82,15 @@ class DisplayHelp:
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
 
-        # closes help dialogue (used by button and x at top of dialogue)
-        def close_help(self, partner):
-            # put help button back to normal
-            partner.to_help_button.config(state=NORMAL)
-            self.help_box.destroy()
-
-
+    # closes help dialogue (used by button and x at top of dialogue)
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 # main routine
-
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
